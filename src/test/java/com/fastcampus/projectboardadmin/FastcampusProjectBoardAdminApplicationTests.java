38f7d1d17006c73fae567349e7d2597f1e9c6c34
gks930620 <gks930620@naver.com>
2024-03-08 16:57:26 +0900

    #4- 스프링 부트 프로젝트 초기화

---
+++ src/test/java/com/fastcampus/projectboardadmin/FastcampusProjectBoardAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fastcampus.projectboardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FastcampusProjectBoardAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
